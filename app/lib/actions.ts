c2356fe9237e1eb0bd75192b68b157355dc73559
Uyen <62287030+bumblebeebop@users.noreply.github.com>
2024-07-18 14:51:05 +0100

    Complete Chapter 12 Mutating Data with Server Actions

---
+++ app/lib/actions.ts
@@ -0,0 +1,69 @@
+"use server";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+/* Server Actions - async functions executed on the server, using 'use server' directive
+    Secure: protect against web attacks, secure data and ensure authorized access
+    Can be called in both Client and Server components
+    Progressive enhancement: allow users to interact with the form and submit data even if JavaScript hasn't been loaded or fails to load
+    Deeply intergrated with NextJS caching - revalidate the associated cache using APIs like revalidatePath() and revalidateTag()
+
+   See: Chapter 12 - Mutating Data using Server Actions https://nextjs.org/learn/dashboard-app/mutating-data
+*/
+
+// validate form data types
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  // insert data to database
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
+
+  revalidatePath("/dashboard/invoices"); // clear cache and trigger a new request to the server to always get fresh data
+  redirect("/dashboard/invoices"); // redirect user back to /dashboard/invoices
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+
+  // insert data to database
+  await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id=${id}`;
+  revalidatePath("/dashboard/invoices");
+}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,33 @@
-'use client';
-
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+"use client";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1"); // reset to page 1 whenever user enters a new search input
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300); // only start search once the user has stopped typing (300ms)
+
+  /* When to use the useSearchParams() hook vs. the searchParams prop?
+      You might have noticed you used two different ways to extract search params. Whether you use one or the other depends on whether you're working on the client or the server.
+      <Search> is a Client Component, so you used the useSearchParams() hook to access the params from the client.
+      <Table> is a Server Component that fetches its own data, so you can pass the searchParams prop from the page to the component.
+      As a general rule, if you want to read the params from the client, use the useSearchParams() hook as this avoids having to go back to the server.
+  */
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +36,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        defaultValue={searchParams.get("query")?.toString()} // uncontrolled component - input manages its own state
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
